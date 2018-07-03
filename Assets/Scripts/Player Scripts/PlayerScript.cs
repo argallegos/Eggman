42f66b6f9c7423cf831c433dfcd5a8b43b63f0ea
A <rag93@drexel.edu>
2018-07-03 15:24:00 -0400

    Player controller stuff

---
+++ Assets/Scripts/Player Scripts/PlayerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveController))]
+
+public class PlayerScript : MonoBehaviour {
+
+    [System.Serializable]
+    public class MouseInput
+    {
+        public Vector2 Damping;
+        public Vector2 Sensitivity;
+
+    }
+
+    [SerializeField] float speed;
+    [SerializeField] MouseInput MouseControl;
+
+    InputController playerInput;
+
+    private MoveController m_MoveController;
+    public MoveController MoveController
+    {
+        get
+        {
+            if (m_MoveController == null)
+                m_MoveController = GetComponent<MoveController>();
+            return m_MoveController;
+        }
+    }
+
+    void Awake () {
+        playerInput = GameManager.Instance.InputController;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 direction = new Vector2(playerInput.Vertical * speed, playerInput.Horizontal * speed);
+        MoveController.Move(direction);
+		
+	}
+}
