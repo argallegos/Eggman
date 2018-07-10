fa16294176a507e287f22764c100b8769579ae7d
abangs730 <39225677+abangs730@users.noreply.github.com>
2018-07-10 14:19:15 -0400

    Seperate GM

---
+++ Assets/Scripts/Player Scripts/Game_Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+//Alex Gallegos and Andrew Bangs (In seperate region) Game manager script
+public class Game_Manager : MonoBehaviour {
+
+	
+	#region Andrew Bangs Code
+	void Awake () {		
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	public int PlayerHP;
+	
+	public void ReduceHealth (){
+		PlayerHP -= 1;
+		
+		if (PlayerHP <= 0){
+			StartCoroutine(RestartLevel(1));
+		}
+	}
+	
+	IEnumerator RestartLevel(float delay) {
+		// wait for the delay amount of seconds, by yield-returning a WaitForSeconds object:
+		yield return new WaitForSeconds(delay);
+		// Reload the active scene:
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+	#endregion
+}
