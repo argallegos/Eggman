da3ea93e1176f7f220fff9a826261d7b7be89ed2
mkurp <39270551+mkurp@users.noreply.github.com>
2018-07-17 12:17:43 -0400

    Watermelon bazooka script

--- Assets/Scripts/WatermelonBazooka.cs
+++ Assets/Scripts/WatermelonBazooka.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Maura Kurp shoot watermelon script
 public class WatermelonBazooka : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject projectile;
+    public Rigidbody rb;
+    public Transform muzzlePoint;
+
+    public float projectileSpeed;
+    public float fireTime = 0.33f;
+    public float projectileLifetime = 5.0f;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void SpawnProjectile()
+    {
+        GameObject spawnedProjectile = (GameObject)Instantiate(projectile, muzzlePoint.position, Quaternion.identity);
+        rb.AddForce(muzzlePoint.forward * projectileSpeed);
+        Destroy(projectile, projectileLifetime);
+    }
 }
