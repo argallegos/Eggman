419019b8ec9ba34fe9ead28666c3bff919eb3ba4
mkurp <39270551+mkurp@users.noreply.github.com>
2018-07-17 10:10:13 -0400

    forks

---
+++ Assets/Scripts/ForkController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForkController : MonoBehaviour {
+
+    private PlayerScript player;
+    private bool playerInRange;
+
+    public float playerRange;
+    public float enemySpeed;
+
+	void Start () {
+        player = FindObjectOfType<PlayerScript>();
+	}
+	
+	void Update () {
+        playerInRange = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), playerRange);
+
+        if (playerInRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+        }
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, playerRange);
+    }
+}
