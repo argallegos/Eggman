41ee5597042cad441f58e40d580bfe851fce866a
abangs730 <39225677+abangs730@users.noreply.github.com>
2018-07-19 18:32:34 -0400

    Menus Yay

---
+++ Assets/Scripts/SingletonLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonLoader : MonoBehaviour {
+
+	public GameObject gameManager;
+	
+	void Awake (){
+		if (GameManager.Instance == null){
+			Instantiate(gameManager);
+		}
+	}
+}
